feef1d1600888f4eb60eea2929287f998818a229
Francisco21233 <141580298+Francisco21233@users.noreply.github.com>
2026-03-28 19:47:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ Reglas:
 `;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
